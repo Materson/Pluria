f04b2bd65bcdad536228841715065d1c6969d1a0
Mateusz Szymanowski <mathew.szyman@gmail.com>
2017-06-07 13:01:32 +0200

    ADD: right menu options

--- Interface/Graphic.py
+++ Interface/Graphic.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QDesktopWidget, QGridLayout, QVBoxLayout, QHBoxLayout,QBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QDesktopWidget, QGridLayout, QVBoxLayout, QHBoxLayout,QBoxLayout, QScrollArea, QLineEdit, QLabel
 from PyQt5.QtCore import Qt
 
 
@@ -8,34 +8,47 @@ class Graphic(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.resize(1000,650)
+        self.resize(1000,600)
         self.center()
-        grid = QGridLayout()
-        grid.setSpacing(0)
-        grid.setContentsMargins(0,0,0,0)
-        self.setLayout(grid)
+        self.grid = QGridLayout()
+        self.grid.setSpacing(0)
+        self.grid.setContentsMargins(0,0,0,0)
+        self.setLayout(self.grid)
 
-        llayout = QVBoxLayout()
-        left = QWidget()
-        left.show()
-        left.resize(600, 650)
-        left.setLayout(llayout)
-        grid.addWidget(left, 0,0,1,7)
+        left = QScrollArea()
+        self.grid.addWidget(left, 0,0,1,7)
+        left.setWidgetResizable(True)
+        mapWidget = QWidget(left)
 
-        rlayout= QVBoxLayout()
-        right = QWidget()
-        right.show()
-        right.resize(300,650)
-        right.setLayout(rlayout)
-        grid.addWidget(right, 0,7,1,3)
-        left.setStyleSheet("background-color:blue")
-        right.setStyleSheet("background-color:red")
+        mapLayout = QGridLayout(mapWidget)
+        mapLayout.setContentsMargins(0,0,0,0,)
+        mapLayout.setSpacing(0)
+        mapWidget.setLayout(mapLayout)
 
+        self.initRight()
 
-        btn = QPushButton("Przycisk")
-        btn2 = QPushButton("Przycisk2")
-        llayout.addWidget(btn)
-        rlayout.addWidget(btn2)
+        left.setStyleSheet("background-color:#fff")
+
+        h = 15
+        w = 2
+        buttons = []
+        for i in range(w):
+            row = []
+            for j in range(h):
+                row.append(None)
+            buttons.append(row)
+
+        for i in range(h):
+            for j in range(w):
+                btn = QPushButton("Przycisk("+str(j)+","+str(i)+")")
+                btn.setFlat(False)
+                btn.setFixedWidth(700/w)
+                btn.setFixedHeight(600/h)
+                btn.setMinimumHeight(60)
+                btn.setMinimumWidth(60)
+                mapLayout.addWidget(btn,i,j)
+                buttons[j][i] = btn
+        left.setWidget(mapWidget)
 
         self.setWindowTitle("Pluria")
         self.show()
@@ -45,3 +58,24 @@ class Graphic(QWidget):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def initRight(self):
+        self.rlayout = QVBoxLayout()
+        self.rlayout.setAlignment(Qt.AlignTop)
+        self.right = QWidget()
+        self.right.setLayout(self.rlayout)
+        self.grid.addWidget(self.right, 0, 7, 1, 3)
+        self.right.setStyleSheet("background-color: #9AF")
+
+        widthLabel = QLabel("Szerokosc")
+        inWidth = QLineEdit()
+        heightLabel = QLabel("Wysokosc")
+        inHeight = QLineEdit()
+        btn = QPushButton("Utwórz")
+
+        self.rlayout.addWidget(widthLabel)
+        self.rlayout.addWidget(inWidth)
+        self.rlayout.addWidget(heightLabel)
+        self.rlayout.addWidget(inHeight)
+        self.rlayout.addWidget(btn)
+
