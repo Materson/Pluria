a3dd24e9b586540f29e2b50df56da455caf62d8b
Mateusz Szymanowski <mathew.szyman@gmail.com>
2017-06-07 11:25:41 +0200

    ADD: separate left and right

--- Interface/Graphic.py
+++ Interface/Graphic.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QDesktopWidget, QGridLayout, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QDesktopWidget, QGridLayout, QVBoxLayout, QHBoxLayout,QBoxLayout
 from PyQt5.QtCore import Qt
 
 
@@ -10,22 +10,32 @@ class Graphic(QWidget):
     def initUI(self):
         self.resize(1000,650)
         self.center()
-
         grid = QGridLayout()
+        grid.setSpacing(0)
+        grid.setContentsMargins(0,0,0,0)
         self.setLayout(grid)
 
-        layout = QHBoxLayout()
+        llayout = QVBoxLayout()
         left = QWidget()
         left.show()
         left.resize(600, 650)
-        left.setLayout(layout)
-        p = left.palette()
-        p.setColor(left.backgroundRole(), Qt.red)
-        left.setPalette(p)
-        grid.addWidget(left, 1,0, 1,3)
+        left.setLayout(llayout)
+        grid.addWidget(left, 0,0,1,7)
+
+        rlayout= QVBoxLayout()
+        right = QWidget()
+        right.show()
+        right.resize(300,650)
+        right.setLayout(rlayout)
+        grid.addWidget(right, 0,7,1,3)
+        left.setStyleSheet("background-color:blue")
+        right.setStyleSheet("background-color:red")
+
 
         btn = QPushButton("Przycisk")
-        layout.addWidget(btn)
+        btn2 = QPushButton("Przycisk2")
+        llayout.addWidget(btn)
+        rlayout.addWidget(btn2)
 
         self.setWindowTitle("Pluria")
         self.show()
